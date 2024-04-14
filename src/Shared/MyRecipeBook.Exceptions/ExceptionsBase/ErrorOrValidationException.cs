dcbff171a6d03bb5ce5d8cc7615492d464116eb5
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-04-14 11:28:25 -0300

    adding exceptions and exceptions messages to register user

---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOrValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+public class ErrorOrValidationException : MyRecipeBookException
+{
+	public IList<string> ErrorMessages { get; set; }
+
+    public ErrorOrValidationException(IList<string> erros)
+    {
+        ErrorMessages = erros;
+    }
+}

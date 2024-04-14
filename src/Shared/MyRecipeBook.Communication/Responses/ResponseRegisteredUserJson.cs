f58bca58e83347120cd8e729733fc1fff737fa77
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-04-14 11:20:46 -0300

    feat: adding

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredUserJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseRegisteredUserJson.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyRecipeBook.Communication.Responses;
 public class ResponseRegisteredUserJson
 {
-	public string name { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
 }
 

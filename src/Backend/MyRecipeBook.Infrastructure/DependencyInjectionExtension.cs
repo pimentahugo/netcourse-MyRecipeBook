f495220a9a4774af54e6be17f5732c5f3d7e4ec1
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-04-28 21:40:33 -0300

    add migration with fluent migration and dependency injection in infracstructure project

--- src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -1,35 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Domain.Repositories;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Infrastructure.DataAccess;
 using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+using MyRecipeBook.Infrastructure.Extensions;
+using System.Reflection;
 
 namespace MyRecipeBook.Infrastructure;
 public static class DependencyInjectionExtension
 {
 	public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
 	{
-		AddDbContext(services, configuration);
 		AddRepositories(services);
+		
+		if(configuration.IsUnitTestEnviroment())
+		{
+			return;
+		}
+
+		AddDbContext(services, configuration);
+		AddFluentMigrator(services, configuration);
 	}
 
-	private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+	private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
 	{
-		var connectionString = configuration.GetConnectionString("Connection");
-
 		services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
 		{
-			dbContextOptions.UseSqlServer(connectionString);
+			dbContextOptions.UseSqlServer(configuration.ConnectionString());
 		});
 	}
 
-	private static void AddRepositories(this IServiceCollection services)
+	private static void AddRepositories(IServiceCollection services)
 	{
 		services.AddScoped<IUnitOfWork, UnitOfWork>();
 		services.AddScoped<IUserReadOnlyRepository, UserRepository>();
 		services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+	}
+
+	private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddFluentMigratorCore().ConfigureRunner(options =>
+		{
+			options.AddSqlServer()
+			.WithGlobalConnectionString(configuration.ConnectionString())
+			.ScanIn(Assembly.Load("MyRecipeBook.Infrastructure")).For.All();
+		});
 	}
 }
 
---
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using FluentMigrator.Runner;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Infrastructure.Migrations;
+public class DatabaseMigration
+{
+	public static void Migrate(string connectionString, IServiceProvider serviceProvider)
+	{
+		EnsureDatabaseCreated(connectionString);
+		MigrationDatabase(serviceProvider);
+	}
+
+	private static void EnsureDatabaseCreated(string connectionString)
+	{
+		var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+		var databaseName = connectionStringBuilder.InitialCatalog;
+
+		connectionStringBuilder.Remove("Database");
+
+		using var dbConnection = new SqlConnection(connectionStringBuilder.ConnectionString);
+
+		var parameters = new DynamicParameters();
+		parameters.Add("name", databaseName);
+
+		var records = dbConnection.Query("SELECT * FROM sys.databases WHERE name = @name", parameters);
+
+		if (records.Any() == false)
+			dbConnection.Execute($"CREATE DATABASE {databaseName}");
+	}
+
+	private static void MigrationDatabase(IServiceProvider serviceProvider)
+	{
+		var migrationRunner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+		migrationRunner.ListMigrations();
+
+		migrationRunner.MigrateUp();
+	}
+}
+

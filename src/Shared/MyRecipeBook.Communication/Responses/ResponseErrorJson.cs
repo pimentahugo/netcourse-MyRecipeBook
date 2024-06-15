2e37faf4e33276d2de30692128641c7291c1c21d
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-06-15 18:06:40 -0300

    fix: improving code with sonar cloud metrics

--- src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -2,13 +2,12 @@
 public class ResponseErrorJson
 {
     public IList<string> Errors { get; set; }
+    public bool TokenIsExpired { get; set; }
 
     public ResponseErrorJson(IList<string> errors) 
     {
         Errors = errors;
     }
-
-    public bool TokenIsExpired { get; set; }
 
     public ResponseErrorJson(string error)
     {

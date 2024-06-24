8e2d29a772966f95e6413153076281f5189f5743
pimentahugo <hugopimenta_marinho_machado@hotmail.com>
2024-06-24 14:27:39 -0300

    add: enums, requests, exceptions messages and validator

---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestInstructionJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Requests;
+public class RequestInstructionJson
+{
+    public int Step { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public long RecipeId { get; set; }
+}
